55fad4db0c88921c53a51667c4d5bc96ab8069d6
Ola-Youssef <138146178+Ola-Youssef@users.noreply.github.com>
2024-04-17 21:52:34 +0200

    Add files via upload

---
+++ BookDao.java
@@ -0,0 +1,9 @@
+package com.proxy.core;
+
+import java.util.List;
+
+
+public interface BookDao {
+	List<Book> getAllBooks() throws Exception;
+
+}
---
+++ DatabaseConnection.java
@@ -0,0 +1,9 @@
+package com.proxy.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface DatabaseConnection {
+	 Connection getConnection() throws SQLException;
+
+}
